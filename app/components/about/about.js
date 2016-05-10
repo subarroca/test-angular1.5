7a3a9b83e18c97f186bd8e0bd76c798b1c2d3a7f
Salvador Subarroca <subarroca@gmail.com>
2016-05-10 16:04:46 +0200

    add form with validation

--- app/components/about/about.js
+++ app/components/about/about.js
@@ -5,5 +5,23 @@ angular.module('myApp.about', [
 ])
 
 .component('about', {
-  templateUrl: 'components/about/about.html'
+  templateUrl: 'components/about/about.html',
+  controller: AboutController
 });
+
+
+function AboutController() {
+	var $ctrl = this;
+
+	$ctrl.model = {
+		name: 'Henry Gale',
+		email: 'magician@oz.net',
+		age: 13
+	}
+
+	$ctrl.submitted = false;
+
+	$ctrl.onSubmit = function () {
+		this.submitted = true;
+	}
+}
