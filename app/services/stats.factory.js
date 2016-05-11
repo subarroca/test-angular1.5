61b17ccfdd1d76d8f9971b705490f3931d62e2ec
Salvador Subarroca <subarroca@gmail.com>
2016-05-11 09:33:45 +0200

    modify template to unify with angular2

--- app/services/stats.factory.js
+++ app/services/stats.factory.js
@@ -5,26 +5,6 @@ angular.module('myApp.stats', [
   var statsUrl = 'http://api.idescat.cat/emex/v1/nodes.json?tipus=mun';
 
 
-  // getCities(): Observable<any> {
-  //   return this.loadInfo();
-  // }
-
-  // getCityStats(id: string): Observable<any> {
-  //   return this.http.get(`http://api.idescat.cat/emex/v1/geo/${id}.json`)
-  //   .map((res) => res.json());
-  // }
-
-  // private loadInfo(): Observable<any> {
-  //   if (!this.info$) {
-  //     this.info$ = this.http.get(this.statsUrl)
-  //     .map( (res) => res.json() );
-  //   }
-
-  //   return this.info$;
-  // }
-
-
-
   this.getCities = function () {
     var q = $q.defer();
 
