b9010d11e941609cd7470d44820ff3dcf6d043ba
Salvador Subarroca <subarroca@gmail.com>
2016-05-11 09:05:36 +0200

    add some ngChange and ngStyle performance check

--- app/components/cities/cities.js
+++ app/components/cities/cities.js
@@ -16,8 +16,55 @@ angular.module('myApp.cities', [
 function CitiesController( StatsFactory ) {
 	var $ctrl = this;
 
-	StatsFactory.getCities()
-	.then( function(data) {
-		$ctrl.cities = data.fitxes.v;
-	});
+	var currentColorIndex = -1;
+	var colors = ['transparent', 'wheat'];
+
+
+	function init (){
+		$ctrl.changeStyle();
+
+		console.time('loadCities');
+
+		StatsFactory.getCities()
+		.then( function(data) {
+			console.time('renderCities');
+			$ctrl.cities = data.fitxes.v;
+			console.timeEnd('loadCities');
+
+			setTimeout(
+				function(){
+					console.timeEnd('renderCities');
+				}
+			);
+		});
+	}
+
+
+	$ctrl.onInputChanged = function() {
+		console.time('inputChange');
+
+		setTimeout(
+			function(){
+				console.timeEnd('inputChange');
+			}
+		);
+	}
+
+
+	$ctrl.changeStyle = function(){
+		console.time('styleChange');
+
+		currentColorIndex = (currentColorIndex + 1) % colors.length;
+		$ctrl.currentColor = colors[currentColorIndex];
+
+		setTimeout(
+			function(){
+				console.timeEnd('styleChange');
+			}
+		);
+	}
+
+
+
+	init();
 }
